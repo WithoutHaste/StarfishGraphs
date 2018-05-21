032ccee829fe0fcc3248edad4d0197b323bebd0f
J M <wohaste@gmail.com>
2018-05-20 19:19:14 -0600

    finished graphs for Lia's presentation

---
+++ NetworkGraphs/Range.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkGraphs
+{
+	public struct Range
+	{
+		public float Start;
+		public float End;
+
+		public Range(float s, float e)
+		{
+			Start = s;
+			End = e;
+		}
+
+		public float Span { get { return End - Start; } }
+	}
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Starfish Graphs
+
+Currently only contains a graph creator for a very specific set of data.
